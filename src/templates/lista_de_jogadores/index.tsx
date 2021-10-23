2be84b1f7f457e3f60542463e89f1047150a477d
igor Gomes <gomesoliveirasl02@hotmail.com>
2021-10-23 16:46:53 -0300

    tela, lista de jogadores quase pronta

--- src/templates/lista_de_jogadores/index.tsx
+++ src/templates/lista_de_jogadores/index.tsx
@@ -1,125 +1,194 @@
 import React from "react"
-import { Conteudo, Conteudo_do_jogo_tabela_1, Conteudo_do_jogo_tabela_2, Conteudo_do_jogo_tabela_3, Conteudo_do_jogo_tabela_4, Conteudo_do_jogo_tabela_5, Conteudo_do_jogo_tabela_6, Conteudo_do_jogo_tabela_7, Conteudo_do_jogo_tabela_8, Conteudo_jogos, Informacoes_do_jogo, Principal, Titulo } from "./style"
+import { Conteudo, Tabela_parte1, Conteudo_do_jogo_corpo2, Tabela_de_informacoes_ajuda, Tabela_de_informacoes_ajuda2, Tabela_de_informacoes_conteudo2, Tabela_de_informacoes_conteudo, Conteudo_do_jogo_tabela_1, Conteudo_jogos1, Conteudo_do_jogo_tabela_2, Conteudo_do_jogo_tabela_3, Conteudo_do_jogo_tabela_4, Conteudo_do_jogo_tabela_5, Conteudo_do_jogo_tabela_6, Conteudo_do_jogo_tabela_7, Conteudo_do_jogo_tabela_8, Conteudo_jogos, Informacoes_do_jogo, Principal, Titulo, Tabela_de_informacoes_title, Tabela_de_informacoes_title2, Tabela_parte2, Conteudo_do_jogo_title1, Conteudo_do_jogo_title2, Conteudo_do_jogo_corpo1, Conteudo_do_jogo_title3, Conteudo_do_jogo_corpo3, Conteudo_do_jogo_title4, Conteudo_do_jogo_corpo4, Conteudo_do_jogo_title5, Conteudo_do_jogo_corpo5, Conteudo_do_jogo_corpo8, Conteudo_do_jogo_title8, Conteudo_do_jogo_corpo7, Conteudo_do_jogo_title7, Conteudo_do_jogo_corpo6, Conteudo_do_jogo_title6, Barra_progresso1, Barra_progresso2, Barra_progresso8, Barra_progresso7, Barra_progresso6, Barra_progresso5, Barra_progresso4, Barra_progresso3 } from "./style"
 
 export const Lista_de_jogadores = () => {
     return <Principal>
-        <div>
-            <Titulo>
-                <h2>
-                    lista de jogos
-                </h2>
-            </Titulo>
-            <Conteudo>
-                <Conteudo_jogos>
-                    <Conteudo_do_jogo_tabela_1>
+
+        <Titulo>
+            <tr>
+                LISTA DE JOGOS
+            </tr>
+        </Titulo>
+        <Conteudo>
+            <Conteudo_jogos>
+                <Conteudo_do_jogo_tabela_1>
+                    <Conteudo_do_jogo_title1>
                         <tr>
                             Nome do jogo 1
                         </tr>
+                    </Conteudo_do_jogo_title1>
+                    <Conteudo_do_jogo_corpo1>
                         <tr>
                             4 participantes
                         </tr>
-                    </Conteudo_do_jogo_tabela_1>
-                    <Conteudo_do_jogo_tabela_2>
+                    </Conteudo_do_jogo_corpo1>
+                <Barra_progresso1 className= "progress">
+                </Barra_progresso1>
+                </Conteudo_do_jogo_tabela_1>
+                        
+                <Conteudo_do_jogo_tabela_2>
+                    <Conteudo_do_jogo_title2>
                         <tr>
                             Nome do jogo 2
                         </tr>
+                    </Conteudo_do_jogo_title2>
+                    <Conteudo_do_jogo_corpo2>
                         <tr>
                             10 participantes
                         </tr>
-                    </Conteudo_do_jogo_tabela_2>
-                    <Conteudo_do_jogo_tabela_3>
+                    </Conteudo_do_jogo_corpo2>
+                    <Barra_progresso2 className= "progress">
+                    </Barra_progresso2>
+                </Conteudo_do_jogo_tabela_2>
+                <Conteudo_do_jogo_tabela_3>
+
+                    <Conteudo_do_jogo_title3>
                         <tr>
                             Nome do jogo 3
                         </tr>
+                    </Conteudo_do_jogo_title3>
+                    <Conteudo_do_jogo_corpo3>
                         <tr>
                             5 participantes
                         </tr>
-                    </Conteudo_do_jogo_tabela_3>
-                    <Conteudo_do_jogo_tabela_4>
+                    </Conteudo_do_jogo_corpo3>
+                    <Barra_progresso3 className= "progress">
+                    </Barra_progresso3>
+                </Conteudo_do_jogo_tabela_3>
+
+                <Conteudo_do_jogo_tabela_4>
+                    <Conteudo_do_jogo_title4>
                         <tr>
                             Nome do jogo 4
                         </tr>
+                    </Conteudo_do_jogo_title4>
+                    <Conteudo_do_jogo_corpo4>
                         <tr>
                             7 participantes
                         </tr>
-                    </Conteudo_do_jogo_tabela_4>
-                    <Conteudo_do_jogo_tabela_5>
+                    </Conteudo_do_jogo_corpo4>
+                    <Barra_progresso4 className= "progress">
+
+                    </Barra_progresso4>
+                </Conteudo_do_jogo_tabela_4>
+            </Conteudo_jogos>
+            <Conteudo_jogos1>
+
+                <Conteudo_do_jogo_tabela_5>
+                    <Conteudo_do_jogo_title5>
                         <tr>
                             Nome do jogo 5
                         </tr>
+                    </Conteudo_do_jogo_title5>
+                    <Conteudo_do_jogo_corpo5>
                         <tr>
                             8 participantes
                         </tr>
-                    </Conteudo_do_jogo_tabela_5>
-                    <Conteudo_do_jogo_tabela_6>
+                    </Conteudo_do_jogo_corpo5>
+                    <Barra_progresso5 className= "progress">
+
+                    </Barra_progresso5>
+                </Conteudo_do_jogo_tabela_5>
+                <Conteudo_do_jogo_tabela_6>
+                    <Conteudo_do_jogo_title6>
                         <tr>
                             Nome do jogo 6
                         </tr>
+                    </Conteudo_do_jogo_title6>
+                    <Conteudo_do_jogo_corpo6>
                         <tr>
                             3 participantes
                         </tr>
-                    </Conteudo_do_jogo_tabela_6>
-                    <Conteudo_do_jogo_tabela_7>
+                    </Conteudo_do_jogo_corpo6>
+                    <Barra_progresso6 className= "progress">
+
+                    </Barra_progresso6>
+                </Conteudo_do_jogo_tabela_6>
+                <Conteudo_do_jogo_tabela_7>
+                    <Conteudo_do_jogo_title7>
                         <tr>
                             Nome do jogo 7
                         </tr>
+                    </Conteudo_do_jogo_title7>
+                    <Conteudo_do_jogo_corpo7>
                         <tr>
                             6 participantes
                         </tr>
-                    </Conteudo_do_jogo_tabela_7>
-                    <Conteudo_do_jogo_tabela_8>
+                    </Conteudo_do_jogo_corpo7>
+                    <Barra_progresso7 className= "progress">
+
+                    </Barra_progresso7>
+                </Conteudo_do_jogo_tabela_7>
+                <Conteudo_do_jogo_tabela_8>
+                    <Conteudo_do_jogo_title8>
                         <tr>
                             Nome do jogo 8
                         </tr>
+                    </Conteudo_do_jogo_title8>
+                    <Conteudo_do_jogo_corpo8>
                         <tr>
                             9 participantes
                         </tr>
-                    </Conteudo_do_jogo_tabela_8>
-                </Conteudo_jogos>
-                <Informacoes_do_jogo>
-                    <h3>
-                        CONVERSAS/CHAT
-                    </h3>
+                    </Conteudo_do_jogo_corpo8>
+                    <Barra_progresso8 className= "progress">
 
-                    <tr>
-                        Adiministração 21/2
+                    </Barra_progresso8>
+                </Conteudo_do_jogo_tabela_8>
+            </Conteudo_jogos1>
+            <Informacoes_do_jogo>
+
+                <Tabela_parte1>
+                    <Tabela_de_informacoes_title>
+                        <tr>
+                            CONVERSAS/CHAT
+                        </tr>
+                    </Tabela_de_informacoes_title>
+                    <Tabela_de_informacoes_conteudo>
+                        <tr>
+                            Adiministração 21/2
+                            <tr>
+                                Engenharia 21/2
+                            </tr>
+                            Marketing 21/2
+                            <tr>
+                                ADS 21/2
+                            </tr>
+                        </tr>
+                    </Tabela_de_informacoes_conteudo>
+                    <Tabela_de_informacoes_ajuda>
+                        <p>
+                            visualizar mais chats
+                        </p>
+                    </Tabela_de_informacoes_ajuda>
+                </Tabela_parte1>
+                <Tabela_parte2>
+                    <Tabela_de_informacoes_title2>
+                        <tr>
+                            TIMES/EQUIPES
+                        </tr>
+                    </Tabela_de_informacoes_title2>
+                    <Tabela_de_informacoes_conteudo2>
+                        <tr>
+                            Adiministração 21/2
+                        </tr>
                         <tr>
                             Engenharia 21/2
                         </tr>
-                        Marketing 21/2
+                        <tr>
+                            Marketing 21/2
+                        </tr>
                         <tr>
                             ADS 21/2
                         </tr>
-                    </tr>
-
-                    <tr>
-                        visualizar mais chats
-                    </tr>
-                    <div>
-                        <h4>
-                            TIMES/EQUIPES
-                        </h4>
-                        <tr>
-                        Adiministração 21/2
-                        </tr>
-                        <tr>
-                        Engenharia 21/2
-                        </tr>
-                        <tr>
-                        Marketing 21/2
-                        </tr>
-                        <tr>
-                        ADS 21/2
-                        </tr>
-
-                        <tr>
-                        visualizar mais chats
-                        </tr>
-
-                    </div>
-                </Informacoes_do_jogo>
-            </Conteudo>
-        </div>
+                    </Tabela_de_informacoes_conteudo2>
+                    <Tabela_de_informacoes_ajuda2>
+                        <p>
+                            visualizar mais chats
+                        </p>
+                    </Tabela_de_informacoes_ajuda2>
+                </Tabela_parte2>
+            </Informacoes_do_jogo>
+        </Conteudo>
     </Principal >
 
 }
--- src/templates/lista_de_jogadores/style.ts
+++ src/templates/lista_de_jogadores/style.ts
@@ -1,82 +1,175 @@
 import styled from "styled-components"
+
 export const Principal = styled.div`
 
 `
 export const Titulo = styled.div`
 margin-left: 300px;
 padding: 70px 0;
-position: static;
+margin-top: 25px;
+font-weight: bold;
+font-size: 23px;
+
 
 `
 export const Conteudo = styled.div`
 
 `
 export const Conteudo_jogos = styled.div`
+display: inline-grid;
 margin-left: 300px;
-display: inline-block;
+display: inline-block
 `
 export const Informacoes_do_jogo = styled.div`
-position: absolute;
-  right: 0px;
-  width: 300px;
-  border: 3px solid #73AD21;
-  padding: 20px;
+display: block;
+margin-left: 850px;
+position: absolute; 
+left: 27px; 
+top: 28%
 
 `
 export const Conteudo_do_jogo_tabela_1 = styled.div`
-width: 20%;
-height: 10%;
-border: solid 1px;
-display: inline-block;
+margin-top:0px;
 
 `
 
 export const Conteudo_do_jogo_tabela_2 = styled.div`
-width: 20%;
-height: 10%;
-border: solid 1px;
-display: inline-block;
+
+margin-top:30px;
 `
 
 export const Conteudo_do_jogo_tabela_3 = styled.div`
-width: 20%;
-height: 10%;
-border: solid 1px;
-display: inline-block;
+margin-top:30px;
+
 `
 
 export const Conteudo_do_jogo_tabela_4 = styled.div`
-width: 20%;
-height: 10%;
-border: solid 1px;
-display: inline-block;
+margin-top:30px;
+
 `
 
 export const Conteudo_do_jogo_tabela_5 = styled.div `
-width: 20%;
-height:10%;
-border: solid 1px;
-display: inline-block;
+margin-top:0px;
+
 `
 
 export const Conteudo_do_jogo_tabela_6 = styled.div`
-width: 20%;
-height: 0%;
-border: solid 1px;
-display: inline-block;
+margin-top:30px;
+
 `
 
 export const Conteudo_do_jogo_tabela_7 = styled.div`
-width: 20%;
-height: 10%;
-border: solid 1px;
-display: inline-block; 
+margin-top:30px;
+
 `
 
 export const Conteudo_do_jogo_tabela_8 = styled.div`
-width: 20%;
-height: 10%;
-border: solid 1px;
-display: inline-block;
+margin-top:30px;
+
+`
+export const Conteudo_jogos1 = styled.div` 
+display: inline-grid;
+margin-left: 150px;
+display: inline-block
+`
+export const Tabela_de_informacoes_ajuda = styled.div`
+font-size: 11px
+`
+export const Tabela_de_informacoes_title = styled.div`
+font-size: 17px;
+font-weight: bold;
+`
+export const Tabela_de_informacoes_conteudo = styled.div`
+font-size: 14px;
+margin-top:20px
+`
+export const Tabela_de_informacoes_ajuda2 = styled.div`
+font-size: 11px
+`
+export const Tabela_de_informacoes_title2 = styled.div`
+font-size: 17px;
+font-weight: bold;
+`
+export const Tabela_de_informacoes_conteudo2 = styled.div`
+font-size: 14px;
+margin-top:20px
+`
+export const Tabela_parte1 = styled.div`
+margin-top: 15px
+`
+export const Tabela_parte2 = styled.div`
+margin-top:30px
+`
+
+export const Conteudo_do_jogo_corpo1 = styled.div`
+font-size: 14px;
+`
+export const Conteudo_do_jogo_corpo2 = styled.div`
+font-size: 14px;
+`
+export const Conteudo_do_jogo_corpo3 = styled.div`
+font-size: 14px;
+`
+export const Conteudo_do_jogo_corpo4 = styled.div`
+font-size: 14px;
+`
+export const Conteudo_do_jogo_corpo5 = styled.div`
+font-size: 14px;
+`
+export const Conteudo_do_jogo_corpo6 = styled.div`
+font-size: 14px;
+`
+export const Conteudo_do_jogo_corpo7 = styled.div`
+font-size: 14px;
+`
+export const Conteudo_do_jogo_corpo8 = styled.div`
+font-size: 14px;
+`
+export const Conteudo_do_jogo_title1 = styled.div`
+font-weight: bold;
+`
+export const Conteudo_do_jogo_title2 = styled.div`
+font-weight: bold;
+`
+export const Conteudo_do_jogo_title3 = styled.div`
+font-weight: bold;
+`
+export const Conteudo_do_jogo_title4 = styled.div`
+font-weight: bold;
+`
+export const Conteudo_do_jogo_title5 = styled.div`
+font-weight: bold;
+`
+export const Conteudo_do_jogo_title6 = styled.div`
+font-weight: bold;
+`
+export const Conteudo_do_jogo_title7 = styled.div`
+font-weight: bold;
+`
+export const Conteudo_do_jogo_title8 = styled.div`
+font-weight: bold;
+`
+export const Barra_progresso1 = styled.div`
+width: 25% "aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"
+`
+export const Barra_progresso2 = styled.div`
+
+`
+export const Barra_progresso3 = styled.div`
+
+`
+export const Barra_progresso4 = styled.div`
+
+`
+export const Barra_progresso5 = styled.div`
+
+`
+export const Barra_progresso6 = styled.div`
+
+`
+export const Barra_progresso7 = styled.div`
+
+`
+export const Barra_progresso8 = styled.div`
 
 `
